11c8660c6c3ab480fb6027981a9f7ad67768ea2c
Sean Murdock <scmurdock@gmail.com>
2022-07-02 11:08:52 -0600

    Removed icorrect RDD references

--- lesson-2-spark-essentials/exercises/concept2-reading-and-writing-data-with-spark/starter/rdd_data_inputs_and_outputs.py
+++
@@ -1,9 +0,0 @@
-import pyspark
-from pyspark import SparkConf
-from pyspark.sql import SparkSession
-
-spark = SparkSession \
-    .builder \
-    .appName("Our first Python Spark SQL example") \
-    .getOrCreate()
-
--- lesson-2-spark-essentials/exercises/concept3-data-wrangling-with-spark/starter/rdd_data_wrangling.py
+++
@@ -1,7 +0,0 @@
-# Take care of any imports
-
-
-# Create the Spark Context
-
-
-# Complete the script
