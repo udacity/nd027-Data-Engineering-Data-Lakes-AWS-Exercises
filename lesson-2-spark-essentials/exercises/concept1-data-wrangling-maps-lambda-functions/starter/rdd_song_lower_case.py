02c4d8cf62113d3de479365e1ad11e1291bc566b
v scarlata <valerie.scarlata@udacity.com>
2022-09-12 15:00:20 -0500

    updating starter with guide comments and song list

--- lesson-2-spark-essentials/exercises/concept1-data-wrangling-maps-lambda-functions/starter/rdd_song_lower_case.py
+++ lesson-2-spark-essentials/exercises/concept1-data-wrangling-maps-lambda-functions/starter/rdd_song_lower_case.py
@@ -15,3 +15,36 @@
 import pyspark
 sc = pyspark.SparkContext(appName="maps_and_lazy_evaluation_example")
 
+
+# Starting off with a regular python list
+log_of_songs = [
+        "Despacito",
+        "Nice for what",
+        "No tears left to cry",
+        "Despacito",
+        "Havana",
+        "In my feelings",
+        "Nice for what",
+        "despacito",
+        "All the stars"
+]
+
+# parallelize the log_of_songs to use with Spark
+
+
+# show the original input data is preserved
+
+
+# create a python function to convert strings to lowercase
+def convert_song_to_lowercase(song):
+    return song.lower()
+
+print(convert_song_to_lowercase("Songtitle"))
+
+# use the map function to transform the list of songs with the python function that converts strings to lowercase
+
+
+# Show the original input data is still mixed case
+
+
+# Use lambda functions instead of named functions to do the same map operation
